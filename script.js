6c45369f0db917591da1e12ca7ac788a5257878d
ailujffshhh <rainierestonio14@gmail.com>
2024-09-02 22:52:56 -0700

    asdasd

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function openSpotifyLogin() {
-    const spotifyLoginUrl = "https://accounts.spotify.com/authorize?client_id=2f4438a4873044d195f984a43319e804&response_type=code&redirect_uri=ailujffshhh.github.io/Real//Real&scope=user-read-private user-read-email"
+    const spotifyLoginUrl = "https://accounts.spotify.com/authorize?client_id=2f4438a4873044d195f984a43319e804&response_type=code&redirect_uri=ailujffshhh.github.io/Real&scope=user-read-private user-read-email"
     window.open(spotifyLoginUrl, '_blank');
 }
