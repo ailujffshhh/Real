559fe40ee5c78969fd72abbe313b77f5538cbd2c
ailujffshhh <rainierestonio14@gmail.com>
2024-09-02 23:10:20 -0700

    asdasd

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openSpotifyLogin() {
     const clientId = '2f4438a4873044d195f984a43319e804';
-    const redirectUri = 'https://ailujffshhh.github.io/Real'; 
+    const redirectUri = 'https://ailujffshhh.github.io/Real/dashboard'; 
     const scopes = 'user-read-private user-read-email';
     const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${encodeURIComponent(clientId)}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
     window.location.href = authUrl;
